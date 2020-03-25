c78b3af55cfbf74b45d610774979e390acee86ab
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-03-25 18:14:31 +0530

    initial commit

--- app/src/main/java/com/androidteamiiitdmj/webspace/MainActivity.java
+++ app/src/main/java/com/androidteamiiitdmj/webspace/MainActivity.java
@@ -1,18 +1,37 @@
 package com.androidteamiiitdmj.webspace;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAnalytics mFirebaseAnalytics;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseAnalytics mFirebaseAnalytics;
+        int SPLASH_TIME_OUT = 4000;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        try{
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){
+            Log.i("Title Bar Not Removed","Null Pointer Exception in Removing Title Bar");
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent (MainActivity.this, Home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
