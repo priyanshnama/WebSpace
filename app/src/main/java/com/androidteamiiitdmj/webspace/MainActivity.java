8c616c8b02c917d650af1dcea1ae32feeff9bdf3
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-03-25 15:20:23 +0530

    initial commit

--- app/src/main/java/com/androidteamiiitdmj/webspace/MainActivity.java
+++ app/src/main/java/com/androidteamiiitdmj/webspace/MainActivity.java
@@ -1,10 +1,16 @@
 package com.androidteamiiitdmj.webspace;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 
 public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
