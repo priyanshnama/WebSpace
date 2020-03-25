edd9212ea736680cb46c265ba4057a698fefc690
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-03-25 17:40:55 +0530

    initial commit

--- app/src/main/java/com/androidteamiiitdmj/webspace/MainActivity.java
+++ app/src/main/java/com/androidteamiiitdmj/webspace/MainActivity.java
@@ -2,19 +2,17 @@ package com.androidteamiiitdmj.webspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 
 public class MainActivity extends AppCompatActivity {
-    private AdView mAdView;
+    private FirebaseAnalytics mFirebaseAnalytics;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 }
