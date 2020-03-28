0348ab2b9f5db6ca7e661b061a93e81e47d0f348
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-03-28 18:16:08 +0530

    initial commit

--- app/src/main/java/com/androidteamiiitdmj/webspace/Home.java
+++ app/src/main/java/com/androidteamiiitdmj/webspace/Home.java
@@ -1,29 +1,58 @@
 package com.androidteamiiitdmj.webspace;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class Home extends AppCompatActivity {
+    Button btn_logout;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-        AdView mAdView;
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-        mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
+        Objects.requireNonNull(this.getSupportActionBar()).hide();
+        btn_logout = findViewById(R.id.logout);
+        btn_logout.setOnClickListener(v -> logout());
+
+        //for google authentication
+        {
+            GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                    .Builder()
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build();
+            mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        }
+
+        // to display banner ad
+        {
+            AdView mAdView;
+            MobileAds.initialize(this, initializationStatus -> {
+            });
+            mAdView = findViewById(R.id.adView);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mAdView.loadAd(adRequest);
+        }
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                task -> Toast.makeText(this,"Signed out",Toast.LENGTH_LONG)
+                        .show());
+        btn_logout.setVisibility(View.INVISIBLE);
     }
 }
--- app/src/main/java/com/androidteamiiitdmj/webspace/MainActivity.java
+++ app/src/main/java/com/androidteamiiitdmj/webspace/MainActivity.java
@@ -1,37 +1,127 @@
 package com.androidteamiiitdmj.webspace;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int GOOGLE_SIGN_IN=123;
+    int SPLASH_TIME_OUT = 100;
+    private FirebaseAuth mAuth;
+    ProgressBar progressBar;
+    GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInAccount account;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        FirebaseAnalytics mFirebaseAnalytics;
-        int SPLASH_TIME_OUT = 4000;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        try{
-            this.getSupportActionBar().hide();
-        }
-        catch (NullPointerException e){
-            Log.i("Title Bar Not Removed","Null Pointer Exception in Removing Title Bar");
-        }
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent (MainActivity.this, Home.class);
-                startActivity(homeIntent);
-                finish();
-            }
+        FirebaseAnalytics.getInstance(this);
+        Objects.requireNonNull(this.getSupportActionBar()).hide();
+
+        new Handler().postDelayed(() -> {
+            mAuth = FirebaseAuth.getInstance();
+            if(mAuth.getCurrentUser() == null ) SignInGoogle();
+            else open_home();
         },SPLASH_TIME_OUT);
+    }
+
+    void SignInGoogle(){
+        progressBar = findViewById(R.id.progress_circular);
+        account = null;
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
+                .Builder()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        Log.d("TAG","Google Signin Started");
+        progressBar.setVisibility(View.VISIBLE);
+        Intent signIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signIntent,GOOGLE_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                account = task.getResult(ApiException.class);
+                assert account != null;
+                boolean vaild_email = valid(account.getEmail());
+                if (vaild_email) firebaseAuthWithGoogle(account);
+                else
+                {
+                    account = null;
+                    Log.d("TAG","Use Institute account");
+                    mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                            newtask -> Toast.makeText(this,"Use Your Institute Account",Toast.LENGTH_LONG)
+                                    .show());
+                    SignInGoogle();
+                }
+            } catch (ApiException e) {
+                Log.w("TAG", "Google sign in failed", e);
+            }
+        }
+    }
+
+    private boolean valid(String email) {
+        Pattern p = Pattern.compile("\\w+(@iiitdmj.ac.in)");
+        Matcher m = p.matcher(email);
+        return m.find();
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d("TAG", "firebaseAuthWithGoogle:" + account.getId());
+        Log.d("TAG", "firebaseAuthWithGoogle_email:" + account.getEmail());
+        AuthCredential credential = GoogleAuthProvider
+                .getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()){
+                        Log.d("TAG","signin success");
+                        open_home();
+                    }
+                    else{
+                        Log.w("TAG","signin failure",task.getException());
+                        Toast.makeText(this, "Signin Failed",Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+    }
+
+    private void open_home(){
+        Intent loginIntent = new Intent(MainActivity.this, Home.class);
+        startActivity(loginIntent);
+        //overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        progressBar.setVisibility(View.INVISIBLE);
     }
 }
