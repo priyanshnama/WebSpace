d202a05570cd06f34e6511b2abb19331f5eaac1d
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-04-04 12:08:30 +0530

    google signin ready

--- app/src/main/java/com/androidteamiiitdmj/webspace/ReportActivity.java
+++ app/src/main/java/com/androidteamiiitdmj/webspace/ReportActivity.java
@@ -3,9 +3,11 @@ package com.androidteamiiitdmj.webspace;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,6 +16,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.File;
 
 public class ReportActivity extends AppCompatActivity {
 
@@ -22,12 +28,16 @@ public class ReportActivity extends AppCompatActivity {
     private TextView txt_file_name, txt_status;
     private RadioButton radio_report,radio_suggesstion;
     private ImageView img_file_image;
+    private String title, description, TAG, name, email;
+    private File image_file;
+    private FirebaseUser user;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -46,7 +56,25 @@ public class ReportActivity extends AppCompatActivity {
 
         txt_title.setOnFocusChangeListener(this::validation);
         txt_description.setOnFocusChangeListener(this::validation);
+        btn_submit.setOnClickListener(this::submit);
+        btn_select_image.setOnClickListener(this::select_image);
 
+    }
+
+    private void select_image(View v) {
+    }
+
+    private void submit(View v) {
+        title = txt_title.getText().toString();
+        description = txt_description.getText().toString();
+        if(title.equals(""))showSoftKeyboard(txt_title);
+        else if(description.equals(""))showSoftKeyboard(txt_description);
+        else{
+            if (radio_report.isChecked()) TAG = "Report";
+            else if (radio_suggesstion.isChecked()) TAG = "Suggestion";
+            email = user.getEmail();
+            name = user.getDisplayName();
+        }
     }
 
     private void validation(View v,boolean hasFocus) {
@@ -67,5 +95,14 @@ public class ReportActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    public void showSoftKeyboard(View view) {
+        view.requestFocus();
+        if (view.requestFocus()) {
+            InputMethodManager imm = (InputMethodManager)
+                    getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+        }
     }
 }
