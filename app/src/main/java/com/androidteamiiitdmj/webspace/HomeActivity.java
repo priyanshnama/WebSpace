f599719fa0d49b0549bc08b73679702c5d3ea0d4
Priyansh Nama <54625644+priyanshnama@users.noreply.github.com>
2020-07-11 22:37:15 +0530

    Basic Structure of App is ready and App works fine

--- app/src/main/java/com/androidteamiiitdmj/webspace/HomeActivity.java
+++ app/src/main/java/com/androidteamiiitdmj/webspace/HomeActivity.java
@@ -2,6 +2,7 @@ package com.androidteamiiitdmj.webspace;
 
 import android.app.ActionBar;
 import android.app.Dialog;
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
@@ -12,20 +13,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import com.androidteamiiitdmj.webspace.ui.internet.InternetFragment;
+import com.androidteamiiitdmj.webspace.ui.intranet.IntranetFragment;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
 import com.squareup.picasso.Picasso;
 
@@ -34,6 +40,9 @@ import java.util.Objects;
 public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private AppBarConfiguration mAppBarConfiguration;
+    private Dialog panel;
+    private FirebaseUser user;
+    private String profile_image;
 
     public HomeActivity() {
     }
@@ -43,20 +52,19 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mAuth = FirebaseAuth.getInstance();
-        if(mAuth.getCurrentUser()==null) close_app();
+        user = mAuth.getCurrentUser();
+        if(user==null) close_app();
         //View  btn_settings = findViewById
         // (R.id.action_settings);
 
 
-        String profile_image = Objects.requireNonNull(mAuth.getCurrentUser().getPhotoUrl()).toString();
+        profile_image = Objects.requireNonNull(user.getPhotoUrl()).toString();
         ImageView imgProfilePic = findViewById(R.id.show_panel);
         Picasso.get().load(profile_image).into(imgProfilePic);
 
         findViewById(R.id.search).setOnClickListener(v->startActivity(new Intent(HomeActivity.this,Search.class)));
 
-
-
-
+        set_up_panel();
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -67,24 +75,66 @@ public class HomeActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navigationView, navController);
 
-
-
-
-
-
         findViewById(R.id.navigate).setOnClickListener(this::navigation);
         findViewById(R.id.contain_panel).setOnClickListener(this::show_panel);
 
-        if( isConnectedToInternet())open_fragment_internet();
-        else open_offline();
+        //if( isConnectedToInternet())open_fragment_internet();
+        //else open_offline();
+    }
+
+    private void set_up_panel() {
+        //set up panel
+        panel = new Dialog(HomeActivity.this);
+        panel.setContentView(R.layout.activity_panel);
+        panel.setCancelable(true);
+        Objects.requireNonNull(panel.getWindow()).setLayout(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+
+        //there are a lot of settings, for panel, check them all out!
+        panel.findViewById(R.id.btn_settings).setOnClickListener(this::open_settings);
+        panel.findViewById(R.id.report).setOnClickListener(this::open_Report);
+
+        String profile_name = Objects.requireNonNull(user).getDisplayName();
+        TextView txt_profile_name = panel.findViewById(R.id.profile_name);
+        for (UserInfo userInfo : user.getProviderData()) {
+            if (profile_name == null && userInfo.getDisplayName() != null) {
+                profile_name = userInfo.getDisplayName();
+            }
+        }
+        txt_profile_name.setText(profile_name);
+
+        String profile_email = Objects.requireNonNull(user).getEmail();
+        TextView txt_profile_email = panel.findViewById(R.id.profile_email);
+        txt_profile_email.setText(profile_email);
+
+        ImageView imgProfilePicOfPanel = panel.findViewById(R.id.profile_image);
+        Picasso.get().load(profile_image).into(imgProfilePicOfPanel);
+
+        panel.show();
+        panel.hide();
+    }
+
+    public static void removeOtherAddedFragments(@NonNull AppCompatActivity activity, @NonNull Fragment fragment) {
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        for (androidx.fragment.app.Fragment frag : fragmentManager.getFragments()) {
+            if (frag != null && !frag.equals(fragment) && frag.isAdded()) {
+                fragmentManager.beginTransaction().remove(frag).commit();
+            }
+        }
     }
 
     private void open_offline() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment,new IntranetFragment());
+        fragmentTransaction.commit();
         Toast.makeText(this, "Not connected to internet", Toast.LENGTH_LONG).show();
     }
 
     private void open_fragment_internet() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment,new InternetFragment());
+        fragmentTransaction.commit();
         Toast.makeText(this, "Connected to internet", Toast.LENGTH_LONG).show();
+
     }
 
     private boolean isConnectedToInternet() {
@@ -100,33 +150,6 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void show_panel(View view){
-        //set up panel
-        Dialog panel = new Dialog(HomeActivity.this);
-        panel.setContentView(R.layout.activity_panel);
-        panel.setCancelable(true);
-        Objects.requireNonNull(panel.getWindow()).setLayout(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
-
-        //there are a lot of settings, for panel, check them all out!
-        panel.findViewById(R.id.btn_settings).setOnClickListener(this::open_settings);
-        panel.findViewById(R.id.report).setOnClickListener(this::open_Report);
-
-        String profile_name = Objects.requireNonNull(mAuth.getCurrentUser()).getDisplayName();
-        TextView txt_profile_name = panel.findViewById(R.id.profile_name);
-        for (UserInfo userInfo : mAuth.getCurrentUser().getProviderData()) {
-            if (profile_name == null && userInfo.getDisplayName() != null) {
-                profile_name = userInfo.getDisplayName();
-            }
-        }
-        txt_profile_name.setText(profile_name);
-
-        String profile_email = Objects.requireNonNull(mAuth.getCurrentUser()).getEmail();
-        TextView txt_profile_email = panel.findViewById(R.id.profile_email);
-        txt_profile_email.setText(profile_email);
-
-        String profile_image = Objects.requireNonNull(mAuth.getCurrentUser().getPhotoUrl()).toString();
-        ImageView imgProfilePic = panel.findViewById(R.id.profile_image);
-        Picasso.get().load(profile_image).into(imgProfilePic);
-
         //now that the panel is set up, it's time to show it
         panel.show();
         MobileAds.initialize(this, this::task);
